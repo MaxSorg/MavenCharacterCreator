29eac7eed7ccd730c170da0f2a90a8f91c7eb95e
MaxSorg <94195077+MaxSorg@users.noreply.github.com>
2022-04-18 18:54:43 -0400

    Render Attribute bonuses
    Added a page to display the races and their attribute bonuses.

---
+++ react-hooks-frontend/src/components/ListRacesComponent.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import RaceService from '../services/RaceService'
+
+
+const ListRacesComponent = () => {
+
+    const [races, setRaces] = useState([])
+
+    useEffect(() => {
+        
+        getAllRaces();
+
+    }, [])    
+    
+
+    
+
+    const getAllRaces = () =>{
+        RaceService.getAllRaces().then((response) => {
+            setRaces(response.data)
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    return (
+        <div className = "container">
+            <h2>List of Races</h2>
+            <table className= "table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Race</th>
+                        <th>Strength</th>
+                        <th>Dexterity</th>
+                        <th>Constitution</th>
+                        <th>Intellect</th>
+                        <th>Wisdom</th>
+                        <th>Charisma</th>
+                    </tr>
+                </thead>
+                <tbody>                    
+                {
+                    Object.keys(races).map (
+                        raceNames=>
+                        <tr>
+                            <td>{raceNames}</td>
+                            {
+                                races[raceNames].map (
+                                bonus =>                                                                            
+                                <td>{bonus}</td>                                                       
+                            )
+                        }  
+                    </tr>
+                )
+            }
+                </tbody>  
+            </table>
+        </div>
+    )
+}
+
+export default ListRacesComponent
---
+++ src/main/java/com/example/MavenCharacterCreator/Controller/RaceController.java
@@ -0,0 +1,30 @@
+package com.example.MavenCharacterCreator.Controller;
+
+import com.example.MavenCharacterCreator.CharacterData.Race;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.EnumMap;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/races")
+public class RaceController {
+
+    
+
+    @GetMapping
+    public EnumMap<Race, int[]> getAllRaces() { 
+        EnumMap<Race, int[]> raceMap = new EnumMap<Race, int[]>(Race.class);     
+        for ( Race race : Race.values()) {
+            int[] bonus = race.RaceBonusesFull();
+            raceMap.put(race, bonus);
+        }
+        return raceMap;
+    }
+    
+}
